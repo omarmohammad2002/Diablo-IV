056b9b9fe5c0a843dc0bc35659df1a24c9647304
02omar <67235119+02omar@users.noreply.github.com>
2024-12-15 00:06:43 +0200

    ui updates

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -5,8 +5,12 @@ public class SceneManagerScript : MonoBehaviour
 {
     public GameObject mainMenuPanel;
     public GameObject gameplayPanel;
+    public GameObject optionsPanel;
+    public GameObject developersPanel;
+    public GameObject creditsPanel;
     public GameObject levelSelectionPanel;
     public GameObject selectCharacterPanel;
+    public GameObject selectBarbarianPanel;
 
     public ImageHoverEffect[] hoverEffects;
 
@@ -21,6 +25,12 @@ public class SceneManagerScript : MonoBehaviour
     {
         if (mainMenuPanel != null) mainMenuPanel.SetActive(false);
         if (gameplayPanel != null) gameplayPanel.SetActive(true);
+    }
+
+    public void OnOptionButtonPressed()
+    {
+        if (mainMenuPanel != null) mainMenuPanel.SetActive(false);
+        if (optionsPanel != null) optionsPanel.SetActive(true);
     }
 
     public void OnExitButtonPressed()
@@ -41,10 +51,60 @@ public class SceneManagerScript : MonoBehaviour
         if (levelSelectionPanel != null) levelSelectionPanel.SetActive(true);
     }
 
+    public void OnDevelopersButtonPressed()
+    {
+        optionsPanel.SetActive(false);
+        developersPanel.SetActive(true);
+    }
+
+    public void OnCreditsButtonPressed()
+    {
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
     public void OnBackToMainMenuPressed()
     {
         if (gameplayPanel != null) gameplayPanel.SetActive(false);
+        if (optionsPanel != null) optionsPanel.SetActive(false);
         if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
+
+        // Reset hover states for all buttons
+        foreach (var hoverEffect in hoverEffects)
+        {
+            hoverEffect.ResetHoverState();
+        }
+    }
+
+    public void OnBackToOptionsPressed()
+    {
+        if (developersPanel != null) developersPanel.SetActive(false);
+        if (creditsPanel != null) creditsPanel.SetActive(false);
+        if (optionsPanel != null) optionsPanel.SetActive(true);
+
+        // Reset hover states for all buttons
+        foreach (var hoverEffect in hoverEffects)
+        {
+            hoverEffect.ResetHoverState();
+        }
+    }
+
+    public void OnBackToSelectCharacterPressed()
+    {
+        if (selectBarbarianPanel != null) selectBarbarianPanel.SetActive(false);
+        if (selectCharacterPanel != null) selectCharacterPanel.SetActive(true);
+
+        // Reset hover states for all buttons
+        foreach (var hoverEffect in hoverEffects)
+        {
+            hoverEffect.ResetHoverState();
+        }
+    }
+
+    public void OnBackToChooseLevelPressed()
+    {
+        if (selectCharacterPanel != null) selectCharacterPanel.SetActive(false);
+        if (gameplayPanel != null) gameplayPanel.SetActive(true);
 
         // Reset hover states for all buttons
         foreach (var hoverEffect in hoverEffects)
